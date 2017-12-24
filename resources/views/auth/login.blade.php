2f641ed0d23b12c17b1e9738f8b6d615053a4a13
Valentin-Genauzeau <vgenauzeau@gmail.com>
2017-12-24 17:00:46 +0100

    login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
+                    <div class="panel-heading">Access your account</div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email"
+                                <div class="col-md-12">
+                                    <input placeholder="Email" id="email" type="email" class="form-control" name="email"
                                            value="{{ old('email') }}" required autofocus>
 
                                     @if ($errors->has('email'))
@@ -27,10 +26,10 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="col-md-12">
+                                    <input placeholder="Password" id="password" type="password" class="form-control"
+                                           name="password" required>
 
                                     @if ($errors->has('password'))
                                         <span class="help-block">
@@ -41,25 +40,29 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-md-12">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox"
-                                                   name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                   name="remember" {{ old('remember') ? 'checked' : '' }}> Keep me
+                                            logged in
                                         </label>
+                                        <a class="btn btn-link col-md-offset-5" href="{{ route('password.request') }}">Forgot
+                                            password ?</a>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-success btn-block">
                                         Login
                                     </button>
 
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Forgot Your Password?
-                                    </a>
+                                    <br>
+
+                                    <p>Not a member ?<a class="btn btn-link" href="{{ route('register') }}">Join now</a>
+                                    </p>
                                 </div>
                             </div>
                         </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,19 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Register</div>
+                    <div class="panel-heading">Create your own account</div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label for="name" class="col-md-4 control-label">Name</label>
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control" name="name"
+                                <div class="col-md-12">
+                                    <input placeholder="Username" id="name" type="text" class="form-control" name="name"
                                            value="{{ old('name') }}" required autofocus>
 
                                     @if ($errors->has('name'))
@@ -27,10 +26,9 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email"
+                                <div class="col-md-12">
+                                    <input placeholder="Email" id="email" type="email" class="form-control" name="email"
                                            value="{{ old('email') }}" required>
 
                                     @if ($errors->has('email'))
@@ -42,10 +40,10 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="col-md-12">
+                                    <input placeholder="Password" id="password" type="password" class="form-control"
+                                           name="password" required>
 
                                     @if ($errors->has('password'))
                                         <span class="help-block">
@@ -56,19 +54,24 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
+                                <div class="col-md-12">
+                                    <input placeholder="Confirm password" id="password-confirm" type="password"
+                                           class="form-control"
                                            name="password_confirmation" required>
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-success btn-block">
                                         Register
                                     </button>
+
+                                    <br>
+
+                                    <p>Already a member ?<a class="btn btn-link" href="{{ route('login') }}">Sign in</a>
+                                    </p>
                                 </div>
                             </div>
                         </form>
